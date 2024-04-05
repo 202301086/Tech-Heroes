efa9a327f0e5c51939e1057afa0f426a27d912a6
rajbhanderi98 <162124551+rajbhanderi98@users.noreply.github.com>
2024-04-05 23:52:58 +0530

    add extra word string and check extra function

--- Code/Word_cloud_generator.cpp
+++ Code/Word_cloud_generator.cpp
@@ -4,6 +4,21 @@ using namespace std;
 int total_word = 0;
 int unique_word = 0;
 
+const int n = 136;
+const string arr[n] = {"a", "able", "about", "above", "across", "after", "all", "almost", "also", 
+    "am", "among", "an", "and", "any", "are", "as", "at", "be", "because", 
+    "been", "but", "by", "can", "cannot", "could", "dear", "did", "do", 
+    "does", "either", "else", "ever", "every", "for", "from", "get", "got", 
+    "had", "has", "have", "he", "her", "hers", "him", "his", "how", "however", 
+    "i", "if", "in", "into", "is", "it", "its", "just", "least", "let", "like", 
+    "likely", "may", "me", "might", "most", "must", "my", "neither", "no", 
+    "nor", "not", "of", "off", "often", "on", "only", "or", "other", "our", 
+    "own" , "rather", "said", "say", "says", "she", "should", "since", "so", 
+    "some", "than", "that", "the", "their", "them", "then", "there", "these", 
+    "they", "this", "tis", "to", "too", "twas", "us", "wants", "was", "we", 
+    "were", "what", "when", "where", "which", "while", "who", "whom", "why", 
+    "will", "with", "would", "yet", "you", "your", "yours", "yourself", "yourselves"};
+
 class node
 {
 public:
@@ -12,6 +27,29 @@ public:
     node *next;
     node *pre;
 };
+
+
+bool check_extra(string s){
+
+    bool check = false;
+
+    for(int i=0;i<=n/2;i++){
+        
+        if(arr[i] == s){
+            check = true;
+            break;
+        }
+
+        if(arr[n-i-1] == s){
+            check = true;
+            break;
+        }
+
+    }
+
+    return check;
+
+}
 
 // Function to search for a node with a specific string value within a doubly linked list.
 // It starts searching from both ends (head and last) simultaneously, moving towards the middle of the list.
